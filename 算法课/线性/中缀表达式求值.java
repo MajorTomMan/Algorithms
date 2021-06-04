abba11121006f5f51363317ce0943aa9ef5adcb4
flashnames <765719516@qq.com>
2021-06-04 09:23:31 +0800

    删除中缀表达式中重复栈的代码,将其修正为使用自定义数据结构中的栈

--- 算法课/线性/中缀表达式求值.java
+++ 算法课/线性/中缀表达式求值.java
@@ -1,6 +1,7 @@
+package 线性;
+import Structure.Stack;
 
 
-/* 
 public class 中缀表达式求值{
     public static void main(String[] args) {
         int i=0;
@@ -9,8 +10,8 @@ public class 中缀表达式求值{
         String statement="(1+((2+3)+(4+5)))";
         char[] state=statement.toCharArray();
         Character cr=' ';
-        OpsStack<Character> ops=new OpsStack<Character>();
-        OpsStack<Double> vals=new OpsStack<Double>();
+        Stack<Character> ops=new Stack<Character>();
+        Stack<Double> vals=new Stack<Double>();
         while(i!=state.length){
             if(state[i]=='('){
                 i++;
@@ -56,60 +57,3 @@ public class 中缀表达式求值{
         System.out.println(statement+" 这句表达式计算结果是:"+vals.top.data.saveData);
     }
 }
-
-
-class OpsStack<T> implements IStack<T>{
-    Node<T> top;
-    int size;
-    @Override
-    public T pop() {
-        // TODO Auto-generated method stub
-        T data;
-        data=top.data.saveData;
-        top=top.next;
-        size--;
-        return data;
-    }
-    @Override
-    public void push(T var) {
-        Node<T> node=new Node<T>();
-        if(isEmpty()){
-            Inital(var);
-            return;
-        }
-        createNode(node);
-        node.data.saveData=var;
-        node.next=top;
-        top=node;
-        size++;
-    }
-    @Override
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        if(top==null){
-            return true;
-        }
-        else{ 
-            return false;
-        }
-    }
-    @Override
-    public void Inital(T var) {
-        // TODO Auto-generated method stub
-        Node<T> node=new Node<T>();
-        createNode(node);
-        node.data.saveData=var;
-        top=node;
-        size++;
-    }
-    public int getSize(){
-        return size;
-    }
-    private void createNode(Node<T> node){
-        node.data=new Data<T>();
-    }
-}
-
-
-
-*/
