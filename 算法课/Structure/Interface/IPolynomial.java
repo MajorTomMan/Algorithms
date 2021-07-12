829eedbb0f68a93440cdab2dbc03096f343b0b37
flashnames <765719516@qq.com>
2021-07-12 14:21:30 +0800

    修改数组删除方法和插入方法以及增加一元多项式相关链表

---
+++ 算法课/Structure/Interface/IPolynomial.java
@@ -0,0 +1,9 @@
+package Structure.Interface;
+
+import Structure.Node.Polynomialnode;
+
+public interface IPolynomial{
+    public void Initial(double power,int exp);
+    public void Insert(double power,int exp);
+    public void Show(Polynomialnode node);
+}
--- 算法课/线性/数组.java
+++ 算法课/线性/数组.java
@@ -1,172 +1,62 @@
-/*
-public class 数组 {
-    public static void main(String[] args){
-        int length=16;
-        DataStructure dataStructure=new DataStructure();
-        dataStructure.stulist(length);
-        Student[] stuList= DataStructure.getStuList();
-        for (int i=0;i<length;i++) {
-            Student student=stuList[i];
-            student.setId(i);
-            stuList[i]=student;
+package 线性;
+
+public class 数组{
+    public static void main(String[] args) {
+        int i=0;
+        int[] array=new int[100];
+        while(i!=array.length){
+            array[i]=i;
+            i++;
         }
-        Student student=new Student();
-        student.setId(7);
-        ListOperator listOperator=new ListOperator(stuList, length);
-        listOperator.Insert(4, student);
-        listOperator.Show();
-        listOperator.Delete(4);
-        listOperator.Show();
+        System.out.println("----------------删除前-----------------");
+        array=insert(3,111, array);
+        show(array);
+        System.out.println("----------------删除后-----------------");
+        array=delete(5,array);
+        show(array);
+    }
+    private static int[] insert(int index,int data,int[] array){
+        int i=0;
+        int[] temp=new int[array.length+1];
+        while(i!=array.length){
+            temp[i]=array[i];
+            i++;
+        }
+        i=index;
+        int tempdata=0,nextdata=0,j=i+1;
+        tempdata=temp[i];
+        temp[i]=data;
+        while(j!=temp.length-1){ //使用tempdata和nextdata来保证数组内数据不会丢失,即nextdata为tempdata下一索引的数据
+            nextdata=temp[j];
+            temp[j]=tempdata;
+            j++;
+            tempdata=nextdata;
+        }
+        temp[j++]=nextdata;
+        return temp;
+    }
+    private static int[] delete(int index,int[] array){ //删除和增加相反就行了
+        int i=array.length-1;
+        int[] temp=new int[array.length-1];
+        int tempdata=0,nextdata=0,j=i-1;
+        tempdata=array[i];
+        while(i!=index+1){
+            nextdata=array[j];
+            array[--i]=tempdata;
+            tempdata=nextdata;
+            j--;
+        }
+        i=0;
+        while(i!=array.length-1){
+            temp[i]=array[i];
+            i++;
+        }
+        return temp;
+    }
+    private static void show(int[] array){
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println("长度是:"+array.length);
     }
 }
-
-class Student {
-    private int id;
-    private int age;
-    private String name;
-    private String nativeplace;
-    private char genger;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNativeplace() {
-        return nativeplace;
-    }
-
-    public void setNativeplace(String nativeplace) {
-        this.nativeplace = nativeplace;
-    }
-
-    public char getGenger() {
-        return genger;
-    }
-
-    public void setGenger(char genger) {
-        this.genger = genger;
-    }
-}
-
-class DataStructure {
-    private static Student[] stuList;
-    private int maxlength;
-    private int length;
-
-    public void stulist(int maxlength) {
-        Init(maxlength);
-    }
-
-    public void Init(int maxlength) {
-        stuList = new Student[maxlength];
-        for (int i = 0; i < maxlength; i++) {
-            Student student = new Student();
-            student.setName("");
-            student.setNativeplace("");
-            student.setGenger(' ');
-            stuList[i] = student;
-        }
-        this.maxlength = maxlength;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public static Student[] getStuList() {
-        return stuList;
-    }
-
-    public static void setStuList(Student[] stuList) {
-        DataStructure.stuList = stuList;
-    }
-
-    public int getMaxlength() {
-        return maxlength;
-    }
-
-    public void setMaxlength(int maxlength) {
-        this.maxlength = maxlength;
-    }
-
-    public void setId(int i) {
-    }
-}
-
-class ListOperator {
-    private static Student[] stuList;
-    private static int length;
-
-    public ListOperator(Student[] stuList, int length) {
-        ListOperator.stuList = stuList;
-        ListOperator.length = length;
-    }
-
-    public void Show() {
-        for (Student student : stuList) {
-            System.out.println(student.getId());
-        }
-        System.out.println("-----------------------");
-    }
-
-    public boolean Insert(int index, Student student) {
-        boolean success = false;
-        for (; index < length; index++) {
-            Student Stunext = stuList[index];
-            stuList[index] = student;
-            student = Stunext;
-            success = true;
-        }
-        return success;
-    }
-
-    public boolean Delete(int index) {
-        boolean success = false;
-        while(true) {
-                if(index+1<length){
-                    Student Stunext = stuList[index+1];
-                    stuList[index]=Stunext;
-                    success=true;
-                }
-                else{
-                    Student Stunext=new Student();
-                    Stunext.setId(0);
-                    stuList[index]=Stunext;
-                    break;
-                }
-                index++;
-        }
-        return success;
-    }
-    public static void Update(){
-    
-    }
-    
-}
-
-
-*/
